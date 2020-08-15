c60ce81676c663c5deff85e119fb0e8958ae265e
Piyush Vinchurkar <piyushvinchurkar10@gmail.com>
2020-08-15 11:02:41 +0530

    initial commit

---
+++ plan.md
@@ -0,0 +1,10 @@
+# Plan of Action
+
+- Initialize node.js project
+- Initiaize our first View
+- Create a room id
+- Add the ability to our own video
+- Add ability to allow others to stream their video
+- Create messages
+- Mute Video
+- Pause video
---
+++ public/script.js
@@ -0,0 +1,95 @@
+const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
+});
+
+let myVideoStream;
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectNewUser(userId, stream);
+    });
+  });
+
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+const connectNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
+
+const addVideoStream = (video, stream) => {
+  (video.srcObject = stream),
+    video.addEventListener("loadedmetadata", () => {
+      video.play();
+    });
+  videoGrid.append(video);
+};
+
+let text = $("input");
+
+$("html").keydown((e) => {
+  if (e.which == 13 && text.val().length !== 0) {
+    socket.emit("message", text.val());
+    text.val("");
+  }
+});
+
+socket.on("createMessage", (message) => {
+  console.log("Create mesage", message);
+  $(".messages").append(`<li class="message><b>user</b><br/>${message}</li>`);
+  scrollToBottom();
+});
+
+const scrollToBottom = () => {
+  let d = $(".main__chat_window");
+  d.scrollTop(d.prop("scrollHeight"));
+};
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+};
+
+const setMuteButton = () => {
+  const html = `<i class="fas fa-microphone"></i><span>Mute</span>`;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `<i class=" unmute fas fa-microphone-slash"></i><span>Unmute</span>`;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
